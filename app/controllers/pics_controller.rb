809b1520377c7861d1fc35d06761a631efaff0d1
Setiadi <setiadi.seth@gmail.com>
2023-06-09 14:08:56 +0700

    create input form for pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,22 @@
 class PicsController < ApplicationController
+  before_action :set_pic, only: [:edit, :show, :update, :destroy]
   def index 
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+
+  def set_pic
+    @pic = Pic.find(params[:id])
   end
 end
