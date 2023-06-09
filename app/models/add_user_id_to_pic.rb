78e6ad2a61ddc492c84594bd3e3f7d4f2e7f6d65
Setiadi <setiadi.seth@gmail.com>
2023-06-09 15:00:24 +0700

    Add devise and assosiations with pics

---
+++ app/models/add_user_id_to_pic.rb
@@ -0,0 +1,2 @@
+class AddUserIdToPic < ApplicationRecord
+end
---
+++ test/models/add_user_id_to_pic_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddUserIdToPicTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
